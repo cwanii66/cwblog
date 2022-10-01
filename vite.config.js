a4fa895c3e846004bd40081144a721ad2bfb3ff8
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-10-01 10:53:44 +0800

    chore: add config and deps

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,31 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import Pages from "vite-plugin-pages";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Inspect from 'vite-plugin-inspect'
+import Pages from 'vite-plugin-pages'
+import UnoCSS from 'unocss/vite'
+import Markdown from 'vite-plugin-vue-markdown'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import Icons from 'unplugin-icons/vite'
+import SVG from 'vite-svg-loader'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Pages()],
-});
+  plugins: [
+    vue(),
+    Pages(),
+    UnoCSS(),
+    Markdown({}),
+    Inspect(),
+    AutoImport(),
+    Components(),
+    Icons({
+      defaultClass: 'inline',
+    }),
+    SVG(),
+  ],
+  build: {},
+  ssgOptions: {
+    formatting: 'minify',
+    format: 'cjs',
+  },
+})
